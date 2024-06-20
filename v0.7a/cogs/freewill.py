9cefa4804f13083ca4a4539034d4cfdf9f6547b0
Infinity Iron <77075118+qr0n@users.noreply.github.com>
2024-06-20 01:38:36 -0500

    modified:   .gitignore 	new file:   v0.7a/MyTODO 	modified:   v0.7a/cogs/freewill.py

--- v0.7a/cogs/freewill.py
+++ v0.7a/cogs/freewill.py
@@ -2,6 +2,7 @@ import json
 from discord.ext import commands
 from discord import Message
 from modules.ContextWindow import ContextWindow
+import random
 
 context_window = ContextWindow.context_window
 
@@ -17,14 +18,16 @@ class Freewill(commands.Cog):
         if config["FREEWILL"]["enabled"]:
 
             channel_id = message.channel.id
-
+            text_frequency = config["FREEWILL"]["text_frequency"]
+            reaction_frequency = config["FREEWILL"]["reaction_frequency"]
+            
             if channel_id not in context_window:
                 context_window[message.channel.id] = []
-
             
             if len(context_window[channel_id]) > config["MAX_CONTEXT_WINDOW"]:
                 context_window[channel_id].pop(0)
 
             context_window[message.channel.id].append(f"{message.author.display_name}: {message.content}")
-            
 
+            if random.random() < text_frequency:
+                prompt_plus = "You are now engaging or adding your own thoughts to this conversation, keep your reply as short as possible but make sure it makes sense and is relevant to the current topic. You will never roleplay as someone you are not instructed to roleplay as, you will use the language last used in your context window to make a response. You must not assume the conversation relates to you."
